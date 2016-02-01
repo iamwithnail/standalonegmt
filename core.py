a79fcc273d42e3a240507fde4d5ac9de2ab5c01d
Chris <chris@strike.ly>
2016-02-01 16:05:09 +0000

    Multi keyword filter working - currently on 'AND' on terms in search box.

--- core.py
+++ core.py
@@ -3,10 +3,6 @@ __author__ = 'chris'
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 @app.route('/main')
 def main_list():
     import exceltojson as etj
@@ -16,18 +12,19 @@ def main_list():
 @app.route('/open')
 def open_list():
     import exceltojson as etj
+    #pass True to the open flag
     return render_template('grantspage.html', grant_list=etj.build_json(True))
 
 @app.route('/filter')
 def filtertest():
     import exceltojson as etj
-    return render_template('filtertest.html', grant_list=etj.build_json())
+    return render_template('filter_test.html', grant_list=etj.build_json())
 
 
 @app.route('/test')
 def othertest():
     import exceltojson as etj
-    return render_template('grantspage.html', grant_list=etj.build_json())
+    return render_template('filter_test.html', grant_list=etj.build_json())
 
 if __name__ == '__main__':
     app.debug=True
--- static/filter2.js
+++ static/filter2.js
@@ -1,3 +1,20 @@
 /**
  * Created by chris on 01/02/2016.
  */
+$(document).ready(function(){
+ //add index column with all content.
+ $(".filterable tr:has(td)").each(function(){
+   var t = $(this).text().toLowerCase(); //all row text
+   $("<td class='indexColumn'></td>")
+    .hide().text(t).appendTo(this);
+ });//each tr
+ $("#search").keyup(function(){
+   var s = $(this).val().toLowerCase().split(" ");
+   //show all rows.
+   $(".filterable tr:hidden").show();
+   $.each(s, function(){
+       $(".filterable tr:visible .indexColumn:not(:contains('"
+          + this + "'))").parent().hide();
+   });//each
+ });//key up.
+});//document.ready
